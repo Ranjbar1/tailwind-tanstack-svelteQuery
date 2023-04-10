8298a8b8e77682b9a0e7dba7742f5d0c099edee5
Ranjbar1 <98ranjbar@gmail.com>
2023-04-10 14:45:17 +0430

    Test vercel CICD

--- src/routes/[pokemonID]/+layout.svelte
+++ src/routes/[pokemonID]/+layout.svelte
@@ -1,4 +1,10 @@
 <div>
 	<h1>pokemon layout</h1>
+	<h1>pokemon layout</h1>
+	<h1>pokemon layout</h1>
+	<h1>pokemon layout</h1>
+	<h1>pokemon layout</h1>
+	<h1>pokemon layout</h1>
+	<h1>pokemon layout</h1>
 	<slot />
 </div>
