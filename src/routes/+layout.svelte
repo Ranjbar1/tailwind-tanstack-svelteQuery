93aa30ff16b19f915f43ba1ff2a83f974b9cbe26
Ranjbar1 <98ranjbar@gmail.com>
2023-04-10 15:16:02 +0430

    Integrate vercel Analytics pkg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,9 @@
 	import { browser } from '$app/environment';
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import '../app.css';
-
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 	const queryClient = new QueryClient({
 		defaultOptions: {
 			queries: {
@@ -13,7 +15,13 @@
 </script>
 
 <QueryClientProvider client={queryClient}>
-	<header> header<header/>
-	<slot />
-	<footer>footer<footer/>
-</QueryClientProvider>
+	<header>
+		header
+		<header />
+		<slot />
+		<footer>
+			footer
+			<footer />
+		</footer>
+	</header></QueryClientProvider
+>
