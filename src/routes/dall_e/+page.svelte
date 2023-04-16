e99c4da3df55d2d774cc180113c13544819498a5
Ranjbar1 <98ranjbar@gmail.com>
2023-04-16 17:21:37 +0430

    Design dall E page

--- src/routes/dall_e/+page.svelte
+++ src/routes/dall_e/+page.svelte
@@ -12,13 +12,36 @@
 </script>
 
 <main>
-	<form class=" bg-blue-200 font-semibold rounded-lg py-2 px-4" on:submit={handleFetch}>
+	<form
+		class=" bg-blue-200 font-semibold rounded-lg shadow-blue-200 shadow-sm mb-4 py-2 px-4"
+		on:submit={handleFetch}
+	>
 		<label for="description">Describe the pic you Looking for:</label>
-		<input bind:value={discription} type="text" class="mx-2 rounded" name="description" />
+		<input
+			bind:value={discription}
+			type="text"
+			class="mx-2 font-normal rounded px-2"
+			name="description"
+		/>
 		<label for="count"> Enter pics Count</label>
-		<input bind:value={count} class="rounded mx-2" type="number" max="5" min="0" name="count" />
+		<input
+			bind:value={count}
+			class="rounded text-center mx-2"
+			type="number"
+			max="5"
+			min="0"
+			name="count"
+		/>
 		<button class="text-white bg-blue-700 rounded px-2 py-1" type="submit">Generate</button>
 	</form>
-	{#each images as img}
-		<div><img class="w-56 h-56 object-cover" src={img.url} alt={discription} /></div>{/each}
+	<div
+		class={` flex justify-around rounded-lg shadow-lg   my-auto min-h-full  overflow-hidden border-gray-700`}
+	>
+		{#each images as img}
+			<img
+				class="w-56 h-56 my-4 overflow-hidden rounded-lg m-1 shadow-sm object-cover"
+				src={img.url}
+				alt={discription}
+			/>{/each}
+	</div>
 </main>
